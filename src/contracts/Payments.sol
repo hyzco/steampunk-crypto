e09d04fbf00e65f36432a202d07715b16ce968b4
enesseniceli <31392403+Shew123@users.noreply.github.com>
2022-01-26 21:19:07 +0300

    Update Payments.sol (#9)
    changed import method website link to @openzeppelin method

--- src/contracts/Payments.sol
+++ src/contracts/Payments.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
 contract Payments is PaymentSplitter{
 
@@ -10,7 +10,7 @@ contract Payments is PaymentSplitter{
 }
 
 /**
-["0x0950f5F3eD03C0Bb4bc1159B517bEFD4f64441C3","0x35c074627e787c2f83A8B925AB29ff2805aBbf8D"]
+["0x0950f5F3eD03C0Bb4bc1159B517bEFD4f64441C3","0xD3A68B6B2e184Ce1b325C9f811DbDCcF975eCab4"]
 */
 
 /**
